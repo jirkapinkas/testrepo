edc668738c3cc237ce0e1e95f3d146d99e76a34c
jirkapinkas <jirka.pinkas@gmail.com>
2017-09-13 08:18:18 +0200

    first

---
+++ src/main/java/com/test/Main.java
@@ -0,0 +1,32 @@
+package com.test;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
+import java.io.FileOutputStream;
+
+/**
+ * Created by jirka on 13.9.2017.
+ */
+public class Main {
+
+    private static void addCell(Row row, int index, String value) {
+        Cell cell = row.createCell(index);
+        cell.setCellValue(value);
+    }
+
+    public static void main(String[] args) throws Exception {
+        try (Workbook wb = new HSSFWorkbook();
+             FileOutputStream fileOut = new FileOutputStream("workbook.xls")) {
+            Sheet sheet = wb.createSheet("new sheet");
+            Row row = sheet.createRow(0);
+            addCell(row, 0, "neco");
+            addCell(row, 1, "dalsiho");
+            wb.write(fileOut);
+        }
+    }
+
+}
